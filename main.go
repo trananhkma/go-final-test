a3aea907405217b9732f83162c0c250f258078ec
trananhkma <trananhkma@gmail.com>
2019-02-26 23:48:51 +0700

    fix pattern

--- main.go
+++ main.go
@@ -3,28 +3,66 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"go-final-test/crawler"
 	"net/http"
 	"strings"
 )
 
-type Target interface {
-	GetTitle(*goquery.Document) string
-	GetDescription(*goquery.Document) string
-	GetBody(*goquery.Document) string
-	GetTime(*goquery.Document) string
+type Info struct {
+	title       string
+	description string
+	body        string
+	time        string
 }
 
-func getTarget(url string) interface{} {
-	if strings.Contains(url, "https://thanhnien.vn") {
-		return crawler.ThanhNien{}
+type DataCrawler interface {
+	CrawlData(doc *goquery.Document) Info
+}
+
+type ThanhNienCrawler struct{}
+func (th ThanhNienCrawler) CrawlData(doc *goquery.Document) Info {
+	var info Info
+	info.title = doc.Find(".details__headline").Text()
+	info.description = doc.Find(".sapo").Text()
+	info.body, _ = doc.Find(".cms-body").Html()
+	time := doc.Find(".details__meta .meta time").Text()
+	t := strings.Split(time, " - ")
+	info.time = t[1] + " " + t[0]
+	return info
+}
+
+type DanTriCrawler struct{}
+func (d DanTriCrawler) CrawlData(doc *goquery.Document) Info {
+	var info Info
+	info.title = doc.Find(".fon31").Text()
+	info.description = doc.Find(".fon33").Text()
+	info.body = doc.Find(".fon34").Text()
+	info.time = doc.Find(".fon7").Text()
+	return info
+}
+
+
+func Crawl(domain string, doc *goquery.Document) Info {
+	var crawler DataCrawler
+	switch domain {
+	case "thanhnien":
+		crawler = ThanhNienCrawler{}
+		return crawler.CrawlData(doc)
+	case "dantri":
+		crawler = DanTriCrawler{}
+		return crawler.CrawlData(doc)
 	}
-	return crawler.DanTri{}
+	return Info{}
 }
 
-func Crawler() {
+func getDomain(url string) string {
+	s := strings.Split(url, "://")
+	uri := s[1]
+	s = strings.Split(uri, ".")
+	return s[0]
+}
+
+func CrawlServer() {
 	http.HandleFunc("/crawler", func(response http.ResponseWriter, request *http.Request) {
-		//url := "https://thanhnien.vn/thoi-su/hinh-anh-nguoi-dan-ong-dung-xe-de-di-ve-sinh-giua-duong-cao-toc-gay-phan-no-1037558.html"
 		url, _ := request.URL.Query()["url"]
 		resp, e := http.Get(url[0])
 
@@ -33,33 +71,24 @@ func Crawler() {
 		}
 		defer resp.Body.Close()
 
-		target := getTarget(url[0])
-
 		doc, err := goquery.NewDocumentFromReader(resp.Body)
-
 		if err != nil {
 			panic(err)
 		}
 
-		title := target.GetTitle(doc)
-		fmt.Fprintln(response, "<html><body><h3>Title: ", title, "</h3><p></p>")
+		domain := getDomain(url[0])
+		info := Crawl(domain, doc)
 
-		des := target.GetDescription(doc)
-		fmt.Fprintln(response, "<h3>Desciption: ", des, "</h3><p></p>")
-
-		body := target.GetBody(doc)
+		fmt.Fprintln(response, "<html><body><h3>Title: ", info.title, "</h3><p></p>")
+		fmt.Fprintln(response, "<h3>Desciption: ", info.description, "</h3><p></p>")
 		fmt.Fprintln(response, "<h3>Body: </h3><p></p>")
-		fmt.Fprintln(response, body)
-
-		time := target.GetTime(doc)
-		fmt.Fprintln(response, "<h3>Time: ", time, "</h3><p></p>")
-
+		fmt.Fprintln(response, info.body)
+		fmt.Fprintln(response, "<h3>Time: ", info.time, "</h3><p></p>")
 		fmt.Fprintln(response, "</body></html>")
 	})
-
 	http.ListenAndServe("localhost:6969", nil)
 }
 
 func main() {
-	Crawler()
+	CrawlServer()
 }
